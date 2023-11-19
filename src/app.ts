73fb93394e74c7f460a9519e3c736d40ec4c5ec7
Akash NP <akashnp1998@gmail.com>
2023-11-19 13:44:54 +0400

    chore: run linting

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import Express from 'express';
+import Express from "express";
 
-const app = Express()
+const app = Express();
 
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server Running on port: ${PORT}`)
-})
+  console.log(`Server Running on port: ${PORT}`); // eslint-disable-line no-console
+});
